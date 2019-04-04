23726e50d977bbc93661dced7a4a590e8845cb12
alexus <afidelo@protonmail.com>
2019-04-04 17:17:13 +0200

    included youtube api call & minor changes

---
+++ video-platform-ui/src/apis/YouTube.js
@@ -0,0 +1,9 @@
+import 'axios' from axios;
+
+const KEY = 'AIzaSyC_XPEDhxm7nxRXL7by_TD_TWOuKqDBgM8';
+
+export default axios.create({
+
+baseURL = 'https://www.googleapis.com/youtube/v3';
+
+});
--- video-platform-ui/src/components/SearchBar.js
+++ video-platform-ui/src/components/SearchBar.js
@@ -2,14 +2,30 @@ import React, {Component} from 'react'
 
 export default class SearchBar extends Component {
 
+	//const API_KEY = 'AIzaSyC_XPEDhxm7nxRXL7by_TD_TWOuKqDBgM8';
+
+	state = {
+		term: ''
+	};
+
+	onInputChange = event => this.setState({term: event.target.value});
+	onFormSubmit = event => {
+		event.preventDefault();
+
+		//to do - make sure we call callback from parent component;
+	}
 	render(){
 		return(
 			
 			<div className="search-bar ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label> Video Search </label>
-						<input type="text" />
+						<input 
+							type="text"
+							value={this.state.term} 
+							onChange={this.onInputChange}
+						/>
 					</div>
 				</form>
 			</div>	
