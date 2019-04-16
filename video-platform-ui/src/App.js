e4e77fefbe5e4f0179ec19965381e5fecea0fc0b
alexus <afidelo@protonmail.com>
2019-04-16 20:41:55 +0200

    `docker-compose` fixed, added components & finsihed youtube-alike application

--- video-platform-ui/src/App.js
+++ video-platform-ui/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 import youtube from './apis/youtube';
 
 export default class App extends Component {
@@ -10,6 +11,12 @@ export default class App extends Component {
             selectedVideo: null
           };
 
+  componentDidMount(){
+    this.onTermSubmit('buildings');
+  }
+  componentDidUpdate(){
+    console.log('State of the state', this.state);
+  }        
   onTermSubmit = async term => {
   	console.log('App.js, search term' + term);
   	const response = await youtube.get('/search', 
@@ -18,18 +25,28 @@ export default class App extends Component {
   			q: term
   		}
   	})
-  	this.setState({videos: response.data.items});
+  	this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
 
   }
-  onVideoSelect = (video) => {
-    console.log('From the App!');
-    console.log(video);
+  onVideoSelect = video => {
+    this.setState({selectedVideo: video});
   }
+ 
   render() {
+
     return (
       <div className="ui container">
       	<SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- video-platform-ui/src/components/VideoDetail.js
+++ video-platform-ui/src/components/VideoDetail.js
@@ -1,1 +1,25 @@
-VideoDetail.js
+import React from 'react';
+
+const videoDetail = ({video}) => {
+    if(!video) return <div> Search for a video...</div>
+
+    const videoSource = `https://www.youtube.com/embed/${video.id.videoId}`
+
+    return (
+
+        <div>
+            <div className="ui embed">
+                <iframe title="video player" src={videoSource}></iframe>
+            </div>
+
+            <div className="ui segment">
+                        <h4 className="ui header">{video.snippet.title}</h4>
+                        <p>{video.snippet.description}</p>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default videoDetail;
